fba868221be5eaba21b8287c35138f5a481f5649
SaghiMottop <saghi.paknemat@motopp.nl>
2026-02-27 15:22:30 +0100

    move creat-add function to db_ads & seed

--- routers/ads.py
+++ routers/ads.py
@@ -1,30 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+# Routes for ads
+
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from db.database import get_db
-from db.models import Category
-from db.models import Advertisement
 from schemas.ads import AdCreate, AdPublic
+from db import db_ads
 
 router = APIRouter(prefix="/ads", tags=["ads"])
 
 
 @router.post("", response_model=AdPublic, status_code=status.HTTP_201_CREATED)
-def create_ad(
-        payload: AdCreate,
-        db: Session = Depends(get_db),
-):
-    category = db.query(Category).filter(Category.id == payload.category_id).first()
-    if not category:
-        raise HTTPException(status_code=400, detail="Invalid category_id")
-
-    ad = Advertisement(
-        seller_id=payload.seller_id,
-        category_id=payload.category_id,
-        title=payload.title,
-        description=payload.description,
-        price=payload.price,
-    )
-    db.add(ad)
-    db.commit()
-    db.refresh(ad)
-    return ad
+def create_ad(payload: AdCreate, db: Session = Depends(get_db)):
+    return db_ads.create_ad(db=db, payload=payload)
